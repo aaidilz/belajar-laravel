a0c005acd5c8c967db869952e9c10416b154e1a9
aaidilz <aidil.233040059@gmail.com>
2024-03-21 22:53:03 +0700

    Update view name in index.php

---
+++ resources/views/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.main')
+
+@section('Wave')
+  <div class='air air1'></div>
+  <div class='air air2'></div>
+  <div class='air air3'></div>
+  <div class='air air4'></div>
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Welcome</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/styles.css">
+  </head>
+  <body>
+    <section>
+        <div class="container d-flex flex-column justify-content-center align-items-center" style="height: 75vh;">
+            <img src="images/1.png" width="300">
+            <div class="container mt-3 mx-3">
+                <h1 class="text-center">Welcome to my website!</h1>
+                <p class="text-center">well, the website is actually working :"</p>
+            </div>
+        </div>
+      @yield('Wave')
+    </section>
+  </body>
+</html>
